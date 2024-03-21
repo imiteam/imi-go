bc9c5a0c2c44a0c512fca1123c8db1061cd6fae4
SelimKhekhaev <selim16695@mail.ru>
2024-03-21 04:15:13 +0300

    fix history table last row border radius

--- src/History/Text/TextHistoryPage/HistoryTable/index.tsx
+++ src/History/Text/TextHistoryPage/HistoryTable/index.tsx
@@ -159,104 +159,191 @@ export default function HistoryTable({textHistory}: {textHistory: GetMyChatsQuer
         </TableHeader>
         <TableBody>
           {currentItems.length &&
-            currentItems.map((d) => {
-              return (
-                <TableRow
-                  className="border-b border-b-[#DAE3E9] dark:border-b-[#17181C]  md:h-[62px] lg:h-[62px] xl:h-[72px]"
-                  key={d.id}
-                  id="textHistoryTableRow"
-                >
-                  <TableCell className="w-[40%] pl-[24px] md:w-[60%]">
-                    <div className="flex h-full items-center">
-                      <div className="mr-3 flex h-full items-center justify-center">
-                        <SwitchHistoryTextIcon icon="chat" />
-                      </div>
-                      <div className="flex h-full flex-col">
-                        <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
-                          {d.title}
-                        </span>
-                        <span className=" font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px] ">
-                          {truncateString(d.description!)}
-                        </span>
-                      </div>
-                    </div>
-                  </TableCell>
-                  <TableCell className="w-[25%] md:w-[20%]">
-                    {/* {d.format === 'doc' ? ( */}
+            currentItems.map((d,index) => {
+              if(pageNumbers.length === 1 && (index === 7 || index === 15 || index === 23 || index === 31 || index === 39 || index === 47 || index === 55 || index === 63)) {
+                return (
+                  <TableRow
+                    className="border-b border-b-[#DAE3E9] dark:border-b-[#17181C]  md:h-[62px] lg:h-[62px] xl:h-[72px]"
+                    key={d.id}
+                    id=""
+                  >
+                    <TableCell className="w-[40%] pl-[24px] md:w-[60%] lastTableRowStyleLeft">
                       <div className="flex h-full items-center">
-                        {/* {d.type.type_icon ? (
-                          <div className="mr-3 flex h-full items-center justify-center">
-                            <SwitchHistoryTextIcon icon={d.type.type_icon} />
-                          </div>
-                        ) : (
-                          <div className="w-8"></div>
-                        )} */}
                         <div className="mr-3 flex h-full items-center justify-center">
-                          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g clip-path="url(#clip0_11268_13175)">
-                            <path d="M0.833496 9.63301C0.833496 5.48463 0.833496 3.41047 2.12223 2.12174C3.41096 0.833008 5.48512 0.833008 9.6335 0.833008H10.3668C14.5152 0.833008 16.5894 0.833008 17.8781 2.12174C19.1668 3.41047 19.1668 5.48463 19.1668 9.63301V10.3663C19.1668 14.5147 19.1668 16.5889 17.8781 17.8776C16.5894 19.1663 14.5152 19.1663 10.3668 19.1663H9.6335C5.48512 19.1663 3.41096 19.1663 2.12223 17.8776C0.833496 16.5889 0.833496 14.5147 0.833496 10.3663V9.63301Z" fill="#0B3BEC"/>
-                            <rect x="13.3335" y="5.83301" width="1.66667" height="9.16667" fill="white"/>
-                            <path d="M12.5 5.83301H11.6528L10 11.1306V11.6663H11.1453L12.5 7.8221V5.83301Z" fill="white"/>
-                            <path d="M7.5 5.83301H8.34724L10 11.1306V11.6663H8.85468L7.5 7.8221V5.83301Z" fill="white"/>
-                            <rect width="1.66667" height="9.16667" transform="matrix(-1 0 0 1 6.6665 5.83301)" fill="white"/>
-                            </g>
-                            <defs>
-                            <clipPath id="clip0_11268_13175">
-                            <rect width="20" height="20" fill="white"/>
-                            </clipPath>
-                            </defs>
-                          </svg>
+                          <SwitchHistoryTextIcon icon="chat" />
                         </div>
                         <div className="flex h-full flex-col">
                           <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
-                            IMI чат
+                            {d.title}
                           </span>
-                          <span className=" font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px]">
-                            {d.description}
+                          <span className=" font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px] ">
+                            {truncateString(d.description!)}
                           </span>
                         </div>
+                      </div>
+                    </TableCell>
+                    <TableCell className="w-[25%] md:w-[20%]">
+                      {/* {d.format === 'doc' ? ( */}
+                        <div className="flex h-full items-center">
+                          {/* {d.type.type_icon ? (
+                            <div className="mr-3 flex h-full items-center justify-center">
+                              <SwitchHistoryTextIcon icon={d.type.type_icon} />
+                            </div>
+                          ) : (
+                            <div className="w-8"></div>
+                          )} */}
+                          <div className="mr-3 flex h-full items-center justify-center">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <g clip-path="url(#clip0_11268_13175)">
+                              <path d="M0.833496 9.63301C0.833496 5.48463 0.833496 3.41047 2.12223 2.12174C3.41096 0.833008 5.48512 0.833008 9.6335 0.833008H10.3668C14.5152 0.833008 16.5894 0.833008 17.8781 2.12174C19.1668 3.41047 19.1668 5.48463 19.1668 9.63301V10.3663C19.1668 14.5147 19.1668 16.5889 17.8781 17.8776C16.5894 19.1663 14.5152 19.1663 10.3668 19.1663H9.6335C5.48512 19.1663 3.41096 19.1663 2.12223 17.8776C0.833496 16.5889 0.833496 14.5147 0.833496 10.3663V9.63301Z" fill="#0B3BEC"/>
+                              <rect x="13.3335" y="5.83301" width="1.66667" height="9.16667" fill="white"/>
+                              <path d="M12.5 5.83301H11.6528L10 11.1306V11.6663H11.1453L12.5 7.8221V5.83301Z" fill="white"/>
+                              <path d="M7.5 5.83301H8.34724L10 11.1306V11.6663H8.85468L7.5 7.8221V5.83301Z" fill="white"/>
+                              <rect width="1.66667" height="9.16667" transform="matrix(-1 0 0 1 6.6665 5.83301)" fill="white"/>
+                              </g>
+                              <defs>
+                              <clipPath id="clip0_11268_13175">
+                              <rect width="20" height="20" fill="white"/>
+                              </clipPath>
+                              </defs>
+                            </svg>
+                          </div>
+                          <div className="flex h-full flex-col">
+                            <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                              IMI чат
+                            </span>
+                            <span className=" font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px]">
+                              {d.description}
+                            </span>
+                          </div>
+                          </div>
+                        {/* </div>
+                      ) : (
+                        <div className="flex h-full items-center">
+                          <div className="mr-3 flex h-full items-center justify-center">
+                            <SwitchHistoryTextIcon icon="imiChat" />
+                          </div>
+                          <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                            {d.type.type_description}
+                          </span>
                         </div>
-                      {/* </div>
-                    ) : (
-                      <div className="flex h-full items-center">
-                        <div className="mr-3 flex h-full items-center justify-center">
-                          <SwitchHistoryTextIcon icon="imiChat" />
-                        </div>
-                        <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
-                          {d.type.type_description}
+                      )} */}
+                    </TableCell>
+                    <TableCell className="w-[25%] md:w-[16%]">
+                      <div className="flex h-full flex-col">
+                        <span className="font-TTNormsRegular leading-[20px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                          {getDifference(d.createdAt)}
+                        </span>
+                        <span className="font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px]">
+                          {formatDate(d.createdAt)}
                         </span>
                       </div>
-                    )} */}
-                  </TableCell>
-                  <TableCell className="w-[25%] md:w-[16%]">
-                    <div className="flex h-full flex-col">
-                      <span className="font-TTNormsRegular leading-[20px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
-                        {getDifference(d.createdAt)}
-                      </span>
-                      <span className="font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px]">
-                        {formatDate(d.createdAt)}
-                      </span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="planSm:w-10px w-[137px] pr-6">
-                    <div className="flex h-full items-center">
-                      <SwitchHistoryTextIcon icon="copy" />
-                      <SwitchHistoryTextIcon icon="edit" />
-                      <SwitchHistoryTextIcon icon="delete" callBack={handleDeleteChat} id={d.id}/>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              )
+                    </TableCell>
+                    <TableCell className="planSm:w-10px w-[137px] pr-6 lastTableRowStyleRight">
+                      <div className="flex h-full items-center">
+                        <SwitchHistoryTextIcon icon="copy" />
+                        <SwitchHistoryTextIcon icon="edit" />
+                        <SwitchHistoryTextIcon icon="delete" callBack={handleDeleteChat} id={d.id}/>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                )
+              } else {
+                return (
+                  <TableRow
+                    className="border-b border-b-[#DAE3E9] dark:border-b-[#17181C]  md:h-[62px] lg:h-[62px] xl:h-[72px]"
+                    key={d.id}
+                    id="textHistoryTableRow"
+                  >
+                    <TableCell className="w-[40%] pl-[24px] md:w-[60%]">
+                      <div className="flex h-full items-center">
+                        <div className="mr-3 flex h-full items-center justify-center">
+                          <SwitchHistoryTextIcon icon="chat" />
+                        </div>
+                        <div className="flex h-full flex-col">
+                          <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                            {d.title}
+                          </span>
+                          <span className=" font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px] ">
+                            {truncateString(d.description!)}
+                          </span>
+                        </div>
+                      </div>
+                    </TableCell>
+                    <TableCell className="w-[25%] md:w-[20%]">
+                      {/* {d.format === 'doc' ? ( */}
+                        <div className="flex h-full items-center">
+                          {/* {d.type.type_icon ? (
+                            <div className="mr-3 flex h-full items-center justify-center">
+                              <SwitchHistoryTextIcon icon={d.type.type_icon} />
+                            </div>
+                          ) : (
+                            <div className="w-8"></div>
+                          )} */}
+                          <div className="mr-3 flex h-full items-center justify-center">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <g clip-path="url(#clip0_11268_13175)">
+                              <path d="M0.833496 9.63301C0.833496 5.48463 0.833496 3.41047 2.12223 2.12174C3.41096 0.833008 5.48512 0.833008 9.6335 0.833008H10.3668C14.5152 0.833008 16.5894 0.833008 17.8781 2.12174C19.1668 3.41047 19.1668 5.48463 19.1668 9.63301V10.3663C19.1668 14.5147 19.1668 16.5889 17.8781 17.8776C16.5894 19.1663 14.5152 19.1663 10.3668 19.1663H9.6335C5.48512 19.1663 3.41096 19.1663 2.12223 17.8776C0.833496 16.5889 0.833496 14.5147 0.833496 10.3663V9.63301Z" fill="#0B3BEC"/>
+                              <rect x="13.3335" y="5.83301" width="1.66667" height="9.16667" fill="white"/>
+                              <path d="M12.5 5.83301H11.6528L10 11.1306V11.6663H11.1453L12.5 7.8221V5.83301Z" fill="white"/>
+                              <path d="M7.5 5.83301H8.34724L10 11.1306V11.6663H8.85468L7.5 7.8221V5.83301Z" fill="white"/>
+                              <rect width="1.66667" height="9.16667" transform="matrix(-1 0 0 1 6.6665 5.83301)" fill="white"/>
+                              </g>
+                              <defs>
+                              <clipPath id="clip0_11268_13175">
+                              <rect width="20" height="20" fill="white"/>
+                              </clipPath>
+                              </defs>
+                            </svg>
+                          </div>
+                          <div className="flex h-full flex-col">
+                            <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                              IMI чат
+                            </span>
+                            <span className=" font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px]">
+                              {d.description}
+                            </span>
+                          </div>
+                          </div>
+                        {/* </div>
+                      ) : (
+                        <div className="flex h-full items-center">
+                          <div className="mr-3 flex h-full items-center justify-center">
+                            <SwitchHistoryTextIcon icon="imiChat" />
+                          </div>
+                          <span className=" font-TTNormsMedium leading-[14px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                            {d.type.type_description}
+                          </span>
+                        </div>
+                      )} */}
+                    </TableCell>
+                    <TableCell className="w-[25%] md:w-[16%]">
+                      <div className="flex h-full flex-col">
+                        <span className="font-TTNormsRegular leading-[20px] text-[#101828] dark:text-[#F5F5F6] md:text-[12px] lg:text-[12px] xl:text-[14px]">
+                          {getDifference(d.createdAt)}
+                        </span>
+                        <span className="font-TTNormsRegular leading-[18px] text-[#667085] dark:text-[#98A2B3] md:text-[12px] lg:text-[12px]">
+                          {formatDate(d.createdAt)}
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell className="planSm:w-10px w-[137px] pr-6">
+                      <div className="flex h-full items-center">
+                        <SwitchHistoryTextIcon icon="copy" />
+                        <SwitchHistoryTextIcon icon="edit" />
+                        <SwitchHistoryTextIcon icon="delete" callBack={handleDeleteChat} id={d.id}/>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                  )
+              }
+            
             })}
         </TableBody>
       </Table>
 
       {pageNumbers.length === 1 ? (
-        <div
-          className="flex h-[68px] w-full items-center justify-between border-t border-t-[#DAE3E9]
-                    bg-white px-6 pb-4 pt-3 dark:border-t-[#17181C] dark:bg-[#21242C] sm:hidden"
-          id="historiesPageTableFooter"
-        ></div>
+        ""
       ) : (
         <div
           className="flex h-[68px] w-full items-center justify-between border-t border-t-[#DAE3E9]
