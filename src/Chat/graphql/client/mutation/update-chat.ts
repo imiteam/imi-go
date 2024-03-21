f7214d4294cb81ad4f5d4cfe4c985e02f1107d52
SelimKhekhaev <selim16695@mail.ru>
2024-03-21 08:01:18 +0300

    add chat description

--- src/Chat/graphql/client/mutation/update-chat.ts
+++ src/Chat/graphql/client/mutation/update-chat.ts
@@ -1,11 +1,12 @@
 import {gql} from 'graphql-request'
 
 export const UpdateChat = gql`
-  mutation UpdateChat($chatId: uuid!, $title: String, $model: String) {
-    update_chats(where: {id: {_eq: $chatId}}, _set: {title: $title, model: $model}) {
+  mutation UpdateChat($chatId: uuid!, $title: String, $model: String, $description: String) {
+    update_chats(where: {id: {_eq: $chatId}}, _set: {title: $title, model: $model, description: $description}) {
       returning {
         id
       }
     }
   }
 `
+ 
