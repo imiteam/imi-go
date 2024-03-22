1862fb34f4ae227e38da7de642d48874e32b2194
SelimKhekhaev <selim16695@mail.ru>
2024-03-22 16:20:42 +0300

    ffffix

--- src/Chat/ChatSection/ChatLayout/index.tsx
+++ src/Chat/ChatSection/ChatLayout/index.tsx
@@ -130,14 +130,12 @@ export const ChatLayout = memo(function ChatLayout(props: {
   //     }) 
   //   }
   // }, [props.id, props.initiateChat])
+  if(document.body){
+    document.body.click();
+    console.log("click")
+  }
+  
 
-  useEffect(() => {
-    //временное решение бага с фризом страницы при перезагрузке
-    const element = document.querySelector('#appelement') as HTMLElement;;
-    if (element) {
-      element.click();
-    }
-  }, []);
   return (
     <div
       className={`z-[20] mb-[12px] flex h-[99%] w-full grow flex-col items-center justify-between bg-[#fff] dark:bg-[#21242C] sm:h-[99.5%] md:rounded-[16px] lg:rounded-[16px] ${margingX}`}
