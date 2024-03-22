52d24e032449ccaf1c83324b395fde281182f371
SelimKhekhaev <selim16695@mail.ru>
2024-03-22 16:34:07 +0300

    not allowed plans button

--- src/common/header/settsDropdown/buttons/PlansButton.tsx
+++ src/common/header/settsDropdown/buttons/PlansButton.tsx
@@ -20,10 +20,10 @@ export const PlansButton = memo(function PlansButton(props: {
         <TooltipTrigger asChild>
           <Link
             onClick={() => props.setIsShowSetts(!props.isShowSetts)}
-            href="/lk/plans"
-            className="flex h-[40px] w-[99%] cursor-pointer select-none items-center rounded-[10px] border-none px-2 py-2.5 font-TTNormsRegular 
+            href="#"
+            className="flex h-[40px] w-[99%] select-none items-center rounded-[10px] border-none px-2 py-2.5 font-TTNormsRegular 
                               text-[14px] leading-[20px] text-[#2D384B] outline-none hover:bg-white focus:bg-white data-[disabled]:pointer-events-none 
-                              data-[disabled]:opacity-50 dark:text-[#F5F5F6] dark:hover:bg-black dark:focus:bg-black"
+                              data-[disabled]:opacity-50 dark:text-[#F5F5F6] dark:hover:bg-black dark:focus:bg-black cursor-not-allowed"
           >
             <ProfilePlansIcon />
             <span className="ml-3">Тарифы и оплата</span>
